9a6d495e6f09770815721685091c1c3afef9de5e
pratikroy <pratikroy2206@gmail.com>
2019-09-07 06:15:13 -0700

    Added list recipe api features

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -40,3 +40,15 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """Create new ingredient"""
         serializer.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrive the recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user)
